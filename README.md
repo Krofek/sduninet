0e2abca84b37c1cbbf23aefd91b1ace835aaf2fe
Krofek <vrabec.matej@gmail.com>
2014-02-04 23:08:06 +0100

    updated a few things

--- README.md
+++ README.md
@@ -1,32 +1,9 @@
 Nvodila:
 ________
-    $ wget https://raw.github.com/Krofek/sduninet/master/sduninet.sh
-
-odpri sduni net in popravi:
-
-spremeni
-
-    wpa=/home/krofek/wpa_supplicant.conf
-
-v
-
-    wpa=/etc/wpa_supplicant.conf
-
-spremeni 
-
-    interface=/home/krofek/interfaces
-
-v
-
-    interface=/etc/network/interfaces
-
-zaženi z:
-
-    $ chmod +x sduninet.sh
-    $ ./sduninet.sh
+    $ wget https://raw.github.com/Krofek/sduninet/master/sduninet.sh && chmod +x sduninet.sh && ./sduninet.sh
 
 sledi navodilom...
 
-na lastno odgovornost :P___________
+na lastno odgovornost...zadeva je nepreverjena.
 
 
--- sduninet.sh
+++ sduninet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-wpa=/home/krofek/wpa_supplicant.conf
-interface=/home/krofek/interfaces
+wpa=/wpa_supplicant.conf
+interface=/etc/network/interfaces
 
 clear
 echo
@@ -12,13 +12,13 @@ echo 'Če ste kaj nastavljali wpa_supplicant za kakršnokoli zadevo,
 jo dobite pod wpa_supplicant.conf.bak, v nasprotnem primeru, gremo dalje :)'
 echo
 if [ -f $wpa ]; then
-	if [ ! -f ${wpa}.bak ]; then
-	echo "$wpa še nima varnostne kopije, pritisni poljubno tipko, da jo ustvari."
-	read -sn 1
-	sudo cp $wpa ${wpa}.bak
-	echo "$wpa varnostna kopija narejena!"
-	echo
-	fi
+    if [ ! -f ${wpa}.bak ]; then
+        echo "$wpa še nima varnostne kopije, pritisni poljubno tipko, da jo ustvari."
+        read -sn 1
+        sudo cp $wpa ${wpa}.bak
+        echo "$wpa varnostna kopija narejena!"
+        echo
+    fi
 fi
 
 echo "1.) Vaše uporabniško ime tipa: janez1@sd.uni-lj.si"
@@ -28,11 +28,11 @@ echo "2.) Vaše geslo"
 read password
 
 if [ -f $wpa ]; then
-sudo rm $wpa
+    sudo rm $wpa
 fi
 
 if [ ! -f $wpa ]; then
-	sudo touch $wpa
+    sudo touch $wpa
 fi
 clear
 echo "Če ste zgrešili uporabniško ime ali geslo prosim da ponovno zaženete skripto:"
@@ -43,13 +43,13 @@ eapol_version=1
 ap_scan=0
 fast_reauth=1
 network={
-    key_mgmt=IEEE8021X
-    eapol_flags=0
-    eap=TTLS
-    anonymous_identity="anonymous@sd.uni-lj.si"
-    phase2="auth=PAP"
-    identity="'$username'"
-    password="'$password'"
+key_mgmt=IEEE8021X
+eapol_flags=0
+eap=TTLS
+anonymous_identity="anonymous@sd.uni-lj.si"
+phase2="auth=PAP"
+identity="'$username'"
+password="'$password'"
 }' | sudo tee -a $wpa
 
 sudo chown root $wpa
@@ -60,32 +60,32 @@ echo "Pritisni poljubno tipko za nadaljevanje..."
 read -sn 1
 clear
 f_interface() {
-egrep -xq "^auto eth0|^iface eth0 inet dhcp|wpa-driver wired|wpa-conf /etc/wpa_supplicant.conf" $interface &>/dev/null
+    egrep -xq "^auto eth0|^iface eth0 inet dhcp|wpa-driver wired|wpa-conf /etc/wpa_supplicant.conf" $interface &>/dev/null
 }
 
 if f_interface; then
-	echo 'Trenutne nastavitve '$interface':'
-	echo
-	cat $interface | egrep "^auto eth0|^iface eth0 inet dhcp|wpa-driver wired|wpa-conf /etc/wpa_supplicant.conf"
+    echo 'Trenutne nastavitve '$interface':'
+    echo
+    cat $interface | egrep "^auto eth0|^iface eth0 inet dhcp|wpa-driver wired|wpa-conf /etc/wpa_supplicant.conf"
 else
-	echo
-	echo ''$interface' še ni nastavljena!'
-	echo
-	if [[ ! f_interface || ! -f $interface ]]; then
-echo 'V '$interface' smo dodali naslednje:'
-echo
-echo 'auto eth0
-iface eth0 inet dhcp
+    echo
+    echo ''$interface' še ni nastavljena!'
+    echo
+    if [[ ! f_interface || ! -f $interface ]]; then
+        echo 'V '$interface' smo dodali naslednje:'
+        echo
+        echo 'auto eth0
+        iface eth0 inet dhcp
         wpa-driver wired
-        wpa-conf /etc/wpa_supplicant.conf' | sudo tee -a $interface 
-fi
+        wpa-conf /etc/wpa_supplicant.conf' | sudo tee -a $interface
+    fi
 fi
 
 echo
 echo "Pritisni poljubno tipko za nadaljevanje..."
 read -sn 1
 
-echo      
+echo
 echo 'To bi moralo biti to, še "networking" restartamo pa bi moralo delovati :)
 (v primeru da vam restartanje netowrkinga zariba sistem, reštartajte mašino :P,
 ubuntu rad heca s takimi neumnostimi...)
